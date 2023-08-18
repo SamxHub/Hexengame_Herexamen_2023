af11f38b08365c34354d7224e96fffd3dbcef3a6
SamxHub <alexandra.haller16@gmail.com>
2023-08-18 15:38:43 +0200

    Wrote BoardView code

--- Assets/Scripts/GameSystem/Views/BoardView.cs
+++ Assets/Scripts/GameSystem/Views/BoardView.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using BoardSystem;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
 
 namespace GameSystem.Views
 {
@@ -18,5 +18,33 @@ namespace GameSystem.Views
     }
     class BoardView
     {
+        public event EventHandler<PositionEventArgs> PositionDrop;
+        public event EventHandler<PositionEventArgs> HoverEnter;
+        public event EventHandler<PositionEventArgs> HoverExit;
+
+        public CardView DroppedCard;
+
+        private Dictionary<Position, TileView> _tileViewsCached = new();
+        private List<Position> _activatedPositions = new();
+
+        public List<Position> ActivatedPositions
+        {
+            set
+            {
+                foreach(var position in _activatedPositions)
+                {
+                    if (_tileViewsCached.ContainsKey(position))
+                        _tileViewsCached[position].Deactivate();
+                }
+                if (value == null)
+                    _activatedPositions = new(0);
+                else
+                    _activatedPositions = value;
+
+                foreach (var position in _activatedPositions)
+                    if (_tileViewsCached.ContainsKey(position))
+                        _tileViewsCached[position].Activate();
+            }            
+        }
     }
 }

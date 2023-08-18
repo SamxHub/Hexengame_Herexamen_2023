10864679e5f3ae5181fa716cc8070186848c7ff7
SamxHub <alexandra.haller16@gmail.com>
2023-08-18 19:25:32 +0200

    Improvements to CardView Drag and Drop Functionality
    Enhance CardView drag and drop functionality

--- Assets/Scripts/GameSystem/Views/CardView.cs
+++ Assets/Scripts/GameSystem/Views/CardView.cs
@@ -6,5 +6,31 @@ namespace GameSystem.Views
 {
     public class CardView : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
     {
+        private CanvasGroup _canvasGroup;
+        private BoardView _boardView;
+
+        private GameObject _copy;
+        private void Awake()
+        {
+            _canvasGroup = gameObject.GetComponent<CanvasGroup>();
+            _boardView = FindObjectOfType<BoardView>();
+        }
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            _copy = Instantiate(this.gameObject, _canvasGroup.transform);
+
+            _canvasGroup.blocksRaycasts = false;
+            _boardView.DroppedCard = this;
+        }
+        public void OnDrag(PointerEventData eventData   )
+        {
+            _copy.transform.position = eventData.position;
+        }
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _canvasGroup.blocksRaycasts = true;
+            _boardView.DroppedCard = null;
+            Destroy(_copy);
+        }
     }
 }

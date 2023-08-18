2c48c4428f7e3cdc15d9be231ba8956e66900fbe
SamxHub <alexandra.haller16@gmail.com>
2023-08-18 16:54:10 +0200

    Board and Piece Event Management Implementation
    Implement Board class for managing piece movements and events

--- Assets/Scripts/BoardSystem/Board.cs
+++ Assets/Scripts/BoardSystem/Board.cs
@@ -4,7 +4,104 @@ using System.Collections.Generic;
 
 namespace BoardSystem
 {
+    public class PieceMovedEventArgs : EventArgs
+    {
+        public Position FromPosition { get; }
+        public Position ToPosition { get; }
+        public PieceView PieceView { get; }
+
+        public PieceMovedEventArgs(Position from, Position To, PieceView pieceView)
+        {
+            FromPosition = from;
+            ToPosition = To;
+            PieceView = pieceView;
+        }
+    }
+    public class PieceTakenEventArgs : EventArgs
+    {
+        public Position HitPosition { get; }
+        public PieceView PieceView { get; }
+        public PieceTakenEventArgs(PieceView pieceView, Position hit)
+        {
+            PieceView = pieceView;
+            HitPosition = hit;
+        }
+    }
+    public class PiecePlacedEventArgs : EventArgs
+    {
+        public PieceView PieceView { get; }
+        public Position PlacedPosition { get; }
+        public PiecePlacedEventArgs(PieceView pieceView, Position position)
+        {
+            PieceView = pieceView;
+            PlacedPosition = position;
+        }
+    }
     class Board
     {
+        public event EventHandler<PieceMovedEventArgs> Moved;
+        public event EventHandler<PieceTakenEventArgs> Taken;
+        public event EventHandler<PiecePlacedEventArgs> Placed;
+
+        private int _radius;
+
+        public Dictionary<Position, PieceView> Pieces = new();
+
+        public Board(int radius)
+        {
+            _radius = radius;
+        }
+        public bool Move(Position from, Position to)
+        {
+            if (!IsValid(from)) return false;
+            if (!IsValid(to)) return false;
+
+            if (Pieces.ContainsKey(to)) return false;
+            if (!Pieces.TryGetValue(from, out var piece)) return false;
+
+            Pieces[to] = piece;
+            Pieces.Remove(from);
+
+            OnMoveObject(new PieceMovedEventArgs(from, to, piece));
+            return true;
+        }
+        public bool Take(Position from)
+        {
+            if (!IsValid(from)) return false;
+            if (!Pieces.TryGetValue(from, out var piece)) return false;
+
+            Pieces.Remove(from);
+
+            OnTakeObject(new PieceTakenEventArgs(piece, from));
+
+            return true;
+        }
+        public bool Place(Position to, PieceView pieceView)
+        {
+            if (!IsValid(to)) return false;
+            if (Pieces.ContainsKey(to)) return false;
+            if (Pieces.ContainsValue(pieceView)) return false;
+
+            Pieces[to] = pieceView;
+
+            OnPlaceObject(new PiecePlacedEventArgs(pieceView, to));
+            return true;
+        }
+        public bool IsValid(Position position) => position.Distance >= -1 * _radius && position.Distance <= _radius;
+        protected virtual void OnMoveObject(PieceMovedEventArgs eventArgs)
+        {
+            var handler = Moved;
+            handler?.Invoke(this, eventArgs); 
+        }
+        protected virtual void OnTakeObject(PieceTakenEventArgs eventArgs)
+        {
+            var handler = Taken;
+            handler?.Invoke(this, eventArgs);
+        }
+        protected virtual void OnPlaceObject(PiecePlacedEventArgs eventArgs)
+        {
+            var handler = Placed;
+            handler?.Invoke(this, eventArgs);
+        }
     }
 }
--- Assets/Scripts/GameSystem/GameState/PlayState.cs
+++ Assets/Scripts/GameSystem/GameState/PlayState.cs
@@ -15,34 +15,92 @@ namespace GameSystem.GameState
         private DeckView _deck;
         public void InitializeScene(UnityEngine.AsyncOperation obj)
         {
+            _boardView = GameObject.FindObjectOfType<BoardView>();
+            _board = new(3);
+
+            _boardView.PositionDrop += Dropped;
+            _boardView.HoverEnter += TileHoverEnter;
+            _boardView.HoverExit += TileHoverExit;
+
+            _engine = new Engine(_board);
+
+            var pieceViews = GameObject.FindObjectsByType<PieceView>();
+            foreach (PieceView p in PieceView)
+            {
+                if (p.Player == PlayerPrefs.Player)
+                    _playerPiece = p;
+
+                _board.Place(p.GridPosition, p);
+            }
+
+            _board.Moved += (s, e) => e.PieceView.MoveTo(e.ToPosition);
+            _board.Taken += (s, e) => e.PieceView.Taken;
+            _board.Placed += (s, e) => e.PieceView.Place(e.PlacePosition);
+
+            _deck = GameObject.FindObjectOfType<DeckView>();
         }
         public override void OnEnter()
         {
-           
+            var asyncOperation = SceneManager.LoadSceneAsync("Cards", LoadSceneMode.Additive);
+            asyncOperation.completed += InitializeScene;
         }
         public override void OnExit()
         {
-            base.OnExit();
+            SceneManager.UnloadSceneAsync("Game");
         }
         public override void OnSuspend()
         {
-            base.OnSuspend();
+            if (_boardView != null)
+            {
+                _boardView.PositionDrop -= Dropped;
+                _boardView.HoverEnter -= TileHoverEnter;
+                _boardView.HoverExit -= TileHoverExit;
+            }
         }
         public override void OnResume()
         {
-            base.OnResume();
-        }
-        private void Dropped(object sender/*,PositionEventArgs eventArgs*/)
-        {
+            if (_boardView != null)
+            {
+                _boardView.PositionDrop -= Dropped;
+                _boardView.PositionDrop += Dropped;
 
-        }
-        private void TileHoverEnter(object sender/*,PositionEventArgs eventArgs*/)
-        {
+                _boardView.HoverEnter -= TileHoverEnter;
+                _boardView.HoverEnter += TileHoverEnter;
 
+                _boardView.HoverExit -= TileHoverExit;
+                _boardView.HoverExit += TileHoverExit;
+            }
         }
-        private void TIleHoverExit(object sender/*,PositionEventArgs eventArgs*/)
+        private void Dropped(object sender, PositionEventArgs eventArgs)
         {
+            var positionHover = eventArgs.Position;
+            var card = eventArgs.Card;
 
+            if (_engine.DoAction(_playerPiece.GridPosition, positionHover))
+                _deck.DeactivateCrd(card);
+
+            _boardView.ActivatedPositions = null;
         }
+        private void TileHoverEnter(object sender, PositionEventArgs eventArgs)
+        {
+            var positionHover = eventArgs.Position;
+            var Card = eventArgs.Card;
+
+            var tiles = _engine.ValidPosition(Card, _playerPiece.GridPosition, positionHover);
+
+            if (tiles == null)
+                return;
+
+            if (tiles.ValidPositions.Contains(positionHover))
+            {
+                _engine.GetActionPositions(positionHover);
+                _boardView.ActivatedPositions = tiles.ActionPositions;
+            }
+            else
+                _boardView.ActivatedPositions = tiles.ValidPositions;
+        }
+        private void TileHoverExit(object sender, PositionEventArgs eventArgs) =>
+           _boardView.ActivatedPositions = null;
+
     }
 }

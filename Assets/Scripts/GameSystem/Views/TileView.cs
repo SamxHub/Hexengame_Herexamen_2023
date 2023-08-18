90bcb67ecf2245a7eeb995aab92acb610a395f64
SamxHub <alexandra.haller16@gmail.com>
2023-08-18 15:38:33 +0200

    Wrote Tileview code

--- Assets/Scripts/GameSystem/Views/TileView.cs
+++ Assets/Scripts/GameSystem/Views/TileView.cs
@@ -7,5 +7,33 @@ namespace GameSystem.Views
 {
     public class TileView : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
     {
+        private BoardView _boardView;
+        [SerializeField] private UnityEvent OnActivate;
+        [SerializeField] private UnityEvent OnDeactivate;
+
+        public Position GridPosition => PositionHelper.GridPositition(transform.position);
+        private void Start()
+        {
+            _boardView = FindObjectOfType<BoardView>();
+        }
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (eventData.pointerDrag == null)
+                return;
+            _boardView.OnTileViewEnter(this);
+        }
+        public void OnDrop(PointerEventData eventData) => _boardView.OnPositionViewDrop(this);
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (eventData.pointerDrag == null) return;
+            _boardView.TileViewExit(this);
+        }
+        private void Awake()
+        {
+            var pos = PositionHelper.GridPosition(transform.position);
+            transform.position = PositionHelper.WorldPosition(pos);
+        }
+        internal void Deactivate() => OnDeactivate?.Invoke();
+        internal void Activate() => OnActivate?.Invoke();
     }
 }

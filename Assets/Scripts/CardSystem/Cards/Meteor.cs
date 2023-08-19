003d94a7606a7dc44a71ea461566285f1ef4b1e3
SamxHub <alexandra.haller16@gmail.com>
2023-08-19 16:49:54 +0200

    MeteorCard Enhancement: Implementation of Meteor Functionality
    Introduce meteor functionality to MeteorCard, expanding area of effect

--- Assets/Scripts/CardSystem/Cards/Meteor.cs
+++ Assets/Scripts/CardSystem/Cards/Meteor.cs
@@ -5,5 +5,39 @@ namespace CardSystem.Cards
 {
     public class MeteorCard : MoveSet
     {
+        public MeteorCard(Board board, Position hoverPos, Position playerPos) : base(board, hoverPos, playerPos)
+        {
+            GetValidPositions(HoverPosition);
+        }
+        public override void GetActionPositions(Position hoverPosition)
+        {
+            ActionPositions.Clear();
+
+            if (ValidPositions.Contains(hoverPosition))
+            {
+                ActionPositions.Add(hoverPosition);
+
+                ActionPositions.AddRange(new MoveSetHelper(hoverPosition, Board)
+                    .NorthEast(1)
+                    .East(1)
+                    .SouthEast(1)
+                    .SouthWest(1)
+                    .West(1)
+                    .NorthWest(1)
+                    .ValidPositions()
+                    );
+            }
+        }
+
+        public override void GetValidPositions(Position position)
+            => ValidPositions.AddRange(new MoveSetHelper(PlayerPosition, Board)
+                .Everything()
+                .ValidPositions());           
+
+        public override List<Position> Positions(Position playerPosition)
+        {
+            GetValidPositions(playerPosition);
+            return ValidPositions;
+        }
     }
 }

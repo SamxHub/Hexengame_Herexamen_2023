43334c043097287abd0fa70e7c9f18f204ba8398
SamxHub <alexandra.haller16@gmail.com>
2023-08-19 15:03:25 +0200

    LaserCard Enhancement: Implementation of Laser Functionality
    Implement laser functionality for LaserCard

--- Assets/Scripts/CardSystem/Cards/Laser.cs
+++ Assets/Scripts/CardSystem/Cards/Laser.cs
@@ -6,5 +6,38 @@ namespace CardSystem.Cards
 {
     class LaserCard : MoveSet
     {
+        public LaserCard(Board board, Position position, Position playerPos) : base(Board, hoverPos, playerPos)
+        {
+            GetValidPositions(PlayerPosition);
+            GetActionPositions(HoverPosition);
+        }
+        public override List<Position> Positions(Position playerPosition)
+        {
+            GetValidPositions(playerPosition);
+            return ValidPositions;
+        }
+        public override void GetValidPositions(Position position)
+        {
+            ValidPositions.AddRange(new MoveSetHelper(PlayerPosition, Board)
+                .NorthEast()
+                .NorthWest()
+                .SouthEast()
+                .SouthWest()
+                .East()
+                .West()
+                .ValidPositions());
+        }
+        public override void GetActionPositions(Position position)
+        {
+            ActionPositions.Clear();
+            MoveSetHelper moveSetAction = new MoveSetHelper(PlayerPosition, Board);
+
+            Vector2Int dir = new((HoverPosition.Q - PlayerPosition.Q), (HoverPosition.R - PlayerPosition.R));
+
+            if (dir == new Vector2Int(0, 0))
+                return;
+
+            ActionPositions.AddRange(moveSetAction.Collect(dir).ValidPositions());
+        }
     }
 }

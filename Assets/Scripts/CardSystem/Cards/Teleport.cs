1922b0231dd26d1163c3dc0337127c198267d84a
SamxHub <alexandra.haller16@gmail.com>
2023-08-19 14:48:07 +0200

    TeleportCard Enhancement: Implementation of Teleportation Functionality
    Implement teleportation functionality for TeleportCard

--- Assets/Scripts/CardSystem/Cards/Teleport.cs
+++ Assets/Scripts/CardSystem/Cards/Teleport.cs
@@ -6,5 +6,29 @@ namespace CardSystem
 {
     public class TeleportCard : MoveSet
     {
+        public TeleportCard(Board board, Position hovPos, Position playerPos) : base(board, hovPos, playerPos)
+            => GetValidPositions(HoverPosition);
+        public override void GetActionPositions(Position position)
+        {
+            ActionPositions.Clear();
+            if (!Board.Pieces.TryGetValue(position, out PieceView piece))
+                ActionPositions.Add(position);
+        }
+        public override void GetValidPositions(Position position)
+        => ValidPositions.AddRange(new MoveSetHelper(PlayerPosition, Board)
+                .Everything().ValidPositions());
+        public override List<Position> Positions(Position playerPosition)
+        {
+            GetValidPositions(playerPosition);
+            return ValidPositions;
+        }
+        internal override bool Execute(Position playerPos, Position hoverPos)
+        {
+            var validPos = Positions(playerPos);
+            if (!validPos.Contains(hoverPos))
+                return false;
+            Board.Move(playerPos, hoverPos);
+            return true;
+        }
     }
 }

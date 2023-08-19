4eb494b84f3c33dec7008ef3570d250c72428419
SamxHub <alexandra.haller16@gmail.com>
2023-08-19 18:06:46 +0200

    StartState Update: Scene Loading and Event Handling Refactor
    Refactor StartState to improve scene loading and event handling

--- Assets/Scripts/GameSystem/GameState/StartState.cs
+++ Assets/Scripts/GameSystem/GameState/StartState.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GameSystem.Views;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace GameSystem.GameState
 {
     class StartState : State
     {
+        private StartView _startView;
+        public override void OnEnter()
+        {
+            var asyncOp = SceneManager.LoadSceneAsync("Start", LoadSceneMode.Additive);
+
+            asyncOp.completed += InitializeScene;
+        }
+        private void InitializeScene(UnityEngine.AsyncOperation operation)
+        {
+            _startView = GameObject.FindObjectOfType<StartView>();
+            if (_startView == null) return;
+
+            _startView.PlayClicked += OnPlayClicked;
+        }
+        private void OnPlayClicked(object sender, EventArgs eventArgs)
+            => StateMachine.MoveTo(GameStates.Play);
+        public override void OnExit()
+        {
+            if (_startView == null)
+                return;
+            _startView.PlayClicked -= OnPlayClicked;
+
+            SceneManager.UnloadSceneAsync("Start");
+        }
     }
 }
